3aaa2131f323342653b1e63edca9a9491891d137
Eduardo Rosas | Applied Math & Data Analysis <rosas.eduardovazquez@gmail.com>
2026-02-13 09:42:01 -0600

    Update Unidad1_ejemplo03_complejos.c

--- Unidad1_ejemplo03_complejos.c
+++ Unidad1_ejemplo03_complejos.c
@@ -14,13 +14,34 @@ typedef struct
 } Complejo;
 
 
-Leer
+Complejo Leer()
+{
+    Complejo numero;
+
+    printf("Lectura de complejos\n\n");
+
+    printf("Parte real: ");
+    scanf("%f", &numero.real);
+
+    printf("Parte imaginaria: ");
+    scanf("%f", &numero.imaginaria);
+
+    return numero;
+}
+
+void Escribir(Complejo )
+{
+    if(numero.imaginaria >= 0)
+        printf("%f + %fi\n", numero.real, numero.imaginaria);
+    else
+        printf("%f + %fi\n", numero.real, numero.imaginaria);
+}
 
 int main()
 {
     
     Complejo a,b,c;
-    
+    a = Leer;
     
 
     return 0;
