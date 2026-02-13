1d94b70483a8307c7493659078767621c23fe074
Eduardo Rosas | Applied Math & Data Analysis <rosas.eduardovazquez@gmail.com>
2026-02-13 09:26:31 -0600

    Update Unidad1_ejemplo03_complejos.c

--- Unidad1_ejemplo03_complejos.c
+++ Unidad1_ejemplo03_complejos.c
@@ -1,1 +1,40 @@
+#include <stdio.h>
+
+//typedef tipo alias;
+//typedef double realDouble;
+
+// typedef struct {variables}  alias;
+
+typedef struct
+{
+    
+    float real;
+    float imaginaria;
+    
+} Complejo;
+
+
+Leer
+
+int main()
+{
+    
+    Complejo a,b,c;
+    
+    
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
