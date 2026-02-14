3ef2fffb293ec79dec2709c96bb3e2e91fab3f74
Eduardo Rosas | Applied Math & Data Analysis <rosas.eduardovazquez@gmail.com>
2026-02-14 16:11:15 +0000

    update

--- num_complejos.c
+++ num_complejos.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
 
+// Creamos la estructura
+typedef struct
+{
+	float real;
+	float imaginaria;
+} Complejo;
+
+// Funcion para ingresar datos
+Complejo Leer()
+{
+	Complejo numero;
+
+	printf("Lectura de complejos: \n");
+	printf("Parte real: \n");
+	scanf("%f", &numero.real);
+	printf("Lectura de parte imaginaria: \n ");
+
+}
+
 int main()
 {
 	printf("Hello World");
