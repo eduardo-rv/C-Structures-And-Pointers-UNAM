9e3872954e3ad252cefa34207a7f355a8d77406c
Eduardo Rosas | Applied Math & Data Analysis <rosas.eduardovazquez@gmail.com>
2026-02-14 17:16:54 +0000

    update

--- num_complejos.c
+++ num_complejos.c
@@ -7,7 +7,7 @@ typedef struct
 	float imaginaria;
 } Complejo;
 
-// Funcion para ingresar datos
+// Funcion para ingresar datos tipo Complejo
 Complejo Leer()
 {
 	Complejo numero;
@@ -16,8 +16,64 @@ Complejo Leer()
 	printf("Parte real: \n");
 	scanf("%f", &numero.real);
 	printf("Lectura de parte imaginaria: \n ");
+	scanf("%f", &numero.imaginaria);
 
+	return numero;
 }
+
+
+// Funcion
+void Escribir(Complejo numero)
+{
+	if(numero.imaginaria > 0)
+	{
+		printf("%.2f+%.2fi\n", numero.real, numero.imaginaria);
+	}
+	else
+	{
+		printf("%.2f%.2fi\n", numero.real, numero.imaginaria);
+	}
+}
+
+
+// Funcion suma
+Complejo suma(Complejo n1, Complejo n2)
+{
+	Complejo resultado;
+
+	resultado.real = n1.real + n2.real;
+	resultado.imaginaria = n1.imaginaria + n2.imaginaria;
+
+	return resultado;
+}
+
+
+// Funcion resta
+Complejo resta (Complejo n1, Complejo n2)
+{
+	Complejo resultado;
+
+	resultado.real = n1.real - n2.real;
+	resultado.imaginaria = n1.imaginaria - n2.imaginaria;
+
+	return resultado;
+}
+
+
+// Funcion producto
+Complejo producto(Complejo n1, Complejo n2)
+{
+	Complejo resultado;
+
+	resultado.real = n1.real*n2.real - n1.imaginaria*n2.imaginaria;
+	resultado.imaginaria = n1.imaginaria*n2.imaginaria + n2.real*n1.imaginaria;
+
+	return resultado;
+}
+
+
+// Funcion division
+
 
 int main()
 {
@@ -25,3 +81,25 @@ int main()
 
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
