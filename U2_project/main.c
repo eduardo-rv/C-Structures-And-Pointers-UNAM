75fd9407f178544fabb50240dd80220d6dc83ccc
Eduardo Rosas <rosas.eduardovazquez@gmail.com>
2026-04-26 09:18:42 -0600

    Add addProduct function and dynamic memory allocationh

--- U2_project/main.c
+++ U2_project/main.c
@@ -8,8 +8,50 @@ typedef struct
 	int quantity;
 }Product;
 
+void addProduct(int *count, Product **inventory)
+{
+	Product *temp = NULL;
+
+	if(count == 0)
+	{
+		temp = (Product *)malloc(1 * sizeof(Product));
+	}
+	else
+	{
+		temp = (Product *)realloc(*inventory ,(*count + 1) * sizeof(Product));  
+	}
+
+	if (temp == NULL)
+	{
+		printf("Memory allocation failed.\n");
+		return;
+	}
+	
+
+	printf("Enter product name: ");
+	scanf("%s", temp[*count].name);
+
+	printf("Enter price: ");
+	scanf("%d", &temp[*count].price);
+
+	printf("Enter quantity: ");
+	scanf("%d", &temp[*count].quantity);
+	
+
+	*inventory = temp;
+	*count = *count + 1;
+
+	printf("Product added successfully\n\n");
+}
+
 int main()
 {
-	
+	int count = 0;
+	Product *inventory = NULL;
+
+	addProduct(&count, &inventory);
+	addProduct(&count, &inventory);
+
+
 	return 0;
 }
